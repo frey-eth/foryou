13585cb34a44cc93144f2a33027f243b0b6cd9f2
Phạm Văn Dương <duongpham321dz@gmail.com>
2023-12-24 23:23:33 +0700

    heh

--- src/App.js
+++ src/App.js
@@ -16,37 +16,27 @@ function App() {
           <i></i>
         </div>
 
-        <h2>From: Ảnh</h2>
+        <h2>From: Duong</h2>
         {showLetter && (
           <div class="typewriter">
             <div className="bg-letter">
               <h2 className="letter">
-                Dear Anh Thư, <br />
-                Hôm nay là ngày em tuyệt vời nhất, ngày em trẻ nhất, tràn đầy
-                năng lượng nhất trong những ngày sau này. Làm sao mà anh lại
-                được ngày ngày được trò chuyện với em vậy ta? Một người luôn
-                mang lại cho anh cảm giác yêu đời, lạc quan, một người quá đỗi
-                xinh đẹp còn vui tính. Hi vọng em sẽ luôn luôn vui vẻ như cách
-                mà em thể hiện ra bên ngoài ^^. Chẳng biết nữa, nhưng mà anh
-                thấy thích em ghê, thích đến mức không thể nào tả được. Chúng
-                mình giống nhau đến lạ thường, anh rất hi vọng cái ngày mình gặp
-                nhau (Lúc đó nhớ trao cho anh một cái ôm nếu em có thể nhen ^^).
-                Hi vọng em là em, mãi xinh đẹp như vậy, mãi cuốn hút như vậy! Em
-                sẽ tuyệt nhất khi là chính mình đó.
+                Chào cậu, <br />
+                Hôm nay là ngày em tuyệt vời nhất, ngày cậu trẻ nhất, tràn đầy
+                năng lượng nhất trong những ngày sau này. Thật cảm thấy may mắn
+                khi tớ quen biết một người như cậu. Hi vọng cậu luôn luôn vui
+                vẻ, lạc quan để đón nhận nhiều thứ tuyệt vời hơn nữa nhé!
                 <br />
-                Dù sau này em có thích ai hay yêu ai thì anh cũng mong rằng em
-                sẽ có được hạnh phúc, em xứng đáng có được nó. Chúc em có một
-                ngày sinh nhật đáng nhớ. Chúc mọi thứ sau này luôn nhẹ nhàng với
-                em.
-                <br />
-                <br />
+                Chúc cậu có một cuộc sống nhẹ nhàng. Làm được những điều cậu
+                muốn làm, yêu được người muốn yêu và hạnh phúc với những gì mình
+                có.
                 <br />
                 Giáng sinh vui vẻ!!
               </h2>
             </div>
           </div>
         )}
-        <h3>To: Cổ</h3>
+        <h3>To: You</h3>
       </div>
 
       <link
